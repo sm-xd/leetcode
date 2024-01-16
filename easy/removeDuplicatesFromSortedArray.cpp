f2307056a3fddd57bfc0b9e43a81d26e84709691
Sky <126008711+sumitmishra01@users.noreply.github.com>
2024-01-16 23:01:36 +0530

    solved remove duplicates

---
+++ easy/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int countIndex = 1;
+        int key = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != key)
+            {
+                key = nums[i];
+                nums[countIndex++] = key;
+            }
+        }
+        return countIndex;
+    }
+};
