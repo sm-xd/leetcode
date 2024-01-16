93f55a171b06f75d15c473dc9b70be84ccf7ca0e
Sky <126008711+sumitmishra01@users.noreply.github.com>
2024-01-16 22:59:38 +0530

    variable renames

--- medium/removeNthNodeFromEndOfList.cpp
+++ medium/removeNthNodeFromEndOfList.cpp
@@ -1,27 +1,33 @@
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
  */
-class Solution {
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = head;
-        ListNode* np = head;
-        while (n) {
-            np = np->next;
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *temp = head;
+        ListNode *nextPointer = head;
+        while (n)
+        {
+            nextPointer = nextPointer->next;
             n--;
         }
-        while (np && np->next) {
-            np = np->next;
+        while (nextPointer && nextPointer->next)
+        {
+            nextPointer = nextPointer->next;
             temp = temp->next;
         }
-        if (!temp->next || !np) {
+        if (!temp->next || !nextPointer)
+        {
             return head->next;
         }
         temp->next = temp->next->next;
