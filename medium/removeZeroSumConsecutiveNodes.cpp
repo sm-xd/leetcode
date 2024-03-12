2f025b53f0843c58643717dd2cda2c73ee01e977
Sky <126008711+sumitmishra01@users.noreply.github.com>
2024-03-12 21:49:46 +0530

    remove zero sum consecutive nodes in linked list

---
+++ medium/removeZeroSumConsecutiveNodes.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        if(head==NULL)
+        return head;
+        ListNode* current=head;
+        int sum=0;
+        while(current!=NULL) 
+        {
+            sum+=current->val;
+            if(sum==0) 
+            {
+                head=current->next;
+                return removeZeroSumSublists(head);
+            }
+            current=current->next;
+        }
+        head->next=removeZeroSumSublists(head->next);
+        return head;
+    }
+};
