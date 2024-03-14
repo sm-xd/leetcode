3a8dbc30139323f558692b0f3cdb5b0be28170cb
Sky <126008711+sumitmishra01@users.noreply.github.com>
2024-03-14 22:33:25 +0530

    binary subarray with sum

---
+++ medium/binarySubarraysWithSum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int i=0,j=0;
+        int sum=0,count=0,freq=0;
+        while(i < nums.size()) {
+            sum += nums[i];
+            if(nums[i] == 1) {
+                freq = 0;
+            }
+            if(sum > goal) {
+                sum -= nums[j];
+                j++;
+            }
+            
+            while(j <= i && sum == goal) {
+                sum -= nums[j];
+                j++;
+                freq++;
+            }
+            count += freq;
+            i++;
+        }
+        return count;
+    }
+};
